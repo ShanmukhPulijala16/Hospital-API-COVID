62e57c10b3d267a8767311485118f0f107a9401c
Shanmukh Pulijala <118909253+ShanmukhPulijala16@users.noreply.github.com>
2024-07-17 12:29:44 +0530

    Update authenticateJWT.js

--- config/authenticateJWT.js
+++ config/authenticateJWT.js
@@ -2,7 +2,6 @@ const jwt = require('jsonwebtoken');
 
 const authenticateJWT = (req, res, next) => {
     const jwtToken = req.cookies.jwt;
-    console.log({ jwtToken });
     if (!jwtToken) {
         return res.status(401).json({ message: "Unauthorized!" });
     }
